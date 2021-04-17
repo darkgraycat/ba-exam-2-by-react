7e10a49b5d299d186647fd0bfcb7f3d052e80f02
darkgraycat <jburyanov@gmail.com>
2021-04-17 20:45:06 +0300

    add logo component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo.jsx'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Header</h1>
+      <Logo />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const style = {
+  backgroundImage: "url('../img/Logo.svg')",
+  backgroundPosition: "center",
+  width: "240px",
+  height: "50px"
+}
+
+const Logo = () => {
+  return (
+    <div style={style}>Logo</div>
+  )
+}
+
+export default Logo
